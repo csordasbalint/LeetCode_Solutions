94030fe8c552bd04631a6647edcdce546ac93a72
balintcsordaas <balintcsordaas@gmail.com>
2023-02-04 01:16:17 +0100

    2185 Solved - Easy

---
+++ LeetCode_Solutions/2185_CountingWordsWithAGivenPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _2185_CountingWordsWithAGivenPrefix
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int countOfWords = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].StartsWith(pref))
+                {
+                    countOfWords++;
+                }
+            }
+
+            return countOfWords;
+        }
+
+        //string[] listOfWords = { "Vater", "Mutter", "Bruder", "Schwester", "Großmutter", "GroßVater", "Multi" };
+        //string prefix = "Mu";
+        //var tmp = PrefixCount(listOfWords, prefix);
+        //Console.WriteLine(tmp);
+    }
+}
