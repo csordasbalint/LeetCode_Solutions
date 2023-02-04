0fa06070db304d26ebf5c408347b1ce5e056e14e
balintcsordaas <balintcsordaas@gmail.com>
2023-02-04 12:34:01 +0100

    1969 Solved - Easy

---
+++ LeetCode_Solutions/1969_NumberOfStringsThatAppearAsSubstringsInWord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _1969_NumberOfStringsThatAppearAsSubstringsInWord
+    {
+        public int NumOfStrings(string[] patterns, string word)
+        {
+            int count = 0;
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                if (word.Contains(patterns[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        //string[] listOfPatterns = { "a", "b", "c", "bc", "daf", "ye", "abc", "abcc" };
+
+        //int tmp = NumOfStrings(listOfPatterns, "abc");
+        //Console.WriteLine(tmp);
+
+    }
+}
