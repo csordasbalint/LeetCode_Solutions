5b53921d158b2d0c609db3bcd487ee868edfaed1
balintcsordaas <balintcsordaas@gmail.com>
2023-02-10 21:34:43 +0100

    1108 Solved - Easy

---
+++ LeetCode_Solutions/1108_DefangingAnIPAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _1108_DefangingAnIPAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+
+        //Console.WriteLine(DefangIPaddr("8.8.8.8"));
+    }
+}
