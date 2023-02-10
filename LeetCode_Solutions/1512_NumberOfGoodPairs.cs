7ecd6e4777a6047bc4b28c71e8b1076f190443d6
balintcsordaas <balintcsordaas@gmail.com>
2023-02-11 00:05:31 +0100

    1512 Solved - Easy

---
+++ LeetCode_Solutions/1512_NumberOfGoodPairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _1512_NumberOfGoodPairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int goodpairs = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (i < j && nums[i] == nums[j])
+                    {
+                        goodpairs++;
+                    }
+                }
+            }
+            return goodpairs;
+        }
+
+        //int[] numbers = { 1, 2, 3, 1, 1, 3 };
+        //Console.WriteLine(NumIdenticalPairs(numbers));
+    }
+}
