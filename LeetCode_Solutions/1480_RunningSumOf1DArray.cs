52563f1e46eb51ce2d11abe308aa195ea8f80307
balintcsordaas <balintcsordaas@gmail.com>
2023-02-11 00:16:31 +0100

    1480 Solved - Easy

---
+++ LeetCode_Solutions/1480_RunningSumOf1DArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _1480_RunningSumOf1DArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] runningsum = new int[nums.Length];
+            int elementValue = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                runningsum[i] = elementValue += nums[i];
+            }
+            return runningsum;
+        }
+
+        //int[] numbers = { 1, 2, 3, 4 };
+        //foreach (var item in RunningSum(numbers))
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
