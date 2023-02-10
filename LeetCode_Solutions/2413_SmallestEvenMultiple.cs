ab64da09e8bd29067a29f3477fd38911fda222f2
balintcsordaas <balintcsordaas@gmail.com>
2023-02-10 12:07:10 +0100

    2413 Solved - Easy

---
+++ LeetCode_Solutions/2413_SmallestEvenMultiple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _2413_SmallestEvenMultiple
+    {
+        int SmallestEvenMultiple(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return 2 * n;
+            }
+        }
+
+        //int output = SmallestEvenMultiple(15);
+        //Console.WriteLine(output);
+    }
+}
