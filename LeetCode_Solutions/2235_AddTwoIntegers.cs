8bd70d886fd3d0d4e05afeceedab219a93a310fc
balintcsordaas <balintcsordaas@gmail.com>
2023-02-10 21:54:42 +0100

    2235 Solved - Easy

---
+++ LeetCode_Solutions/2235_AddTwoIntegers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _2235_AddTwoIntegers
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        //Console.WriteLine(Sum(5,7));
+    }
+}
