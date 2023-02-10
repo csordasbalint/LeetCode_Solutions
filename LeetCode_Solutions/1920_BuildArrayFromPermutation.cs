bf81fa95f8be1a80f5741914d3b39f20848ba645
balintcsordaas <balintcsordaas@gmail.com>
2023-02-10 18:12:25 +0100

    1920 Solved - Easy

---
+++ LeetCode_Solutions/1920_BuildArrayFromPermutation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class _1920_BuildArrayFromPermutation
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+
+            return ans;
+        }
+
+        //int[] tmp = { 0, 2, 1, 5, 3, 4 };
+        //int[] output = BuildArray(tmp);
+        //foreach (var item in output)
+        //{
+        //	Console.WriteLine(item);
+        //}
+    }
+}
