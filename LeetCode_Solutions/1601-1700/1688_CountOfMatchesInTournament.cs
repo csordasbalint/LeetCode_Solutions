5ef66afa1a80e19e077c4e0e70d9eef7baf5ba55
balintcsordaas <balintcsordaas@gmail.com>
2023-02-20 22:31:47 +0100

    1688 Solved - Easy

---
+++ LeetCode_Solutions/1601-1700/1688_CountOfMatchesInTournament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1601_1700
+{
+    internal class _1688_CountOfMatchesInTournament
+    {
+        public int NumberOfMatches(int n)
+        {
+            return n - 1;
+        }
+
+        //Console.WriteLine(NumberOfMatches(7));
+    }
+}
