4905313b4eaa8f73c72ee539c7475707c919b303
balintcsordaas <balintcsordaas@gmail.com>
2023-02-21 15:56:46 +0100

    461 Solved - Easy

---
+++ LeetCode_Solutions/0401-0500/0461_HammingDistance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0401_0500
+{
+    internal class _0461_HammingDistance
+    {
+        public int HammingDistance(int x, int y)
+        {
+            char[] bin1 = Convert.ToString(x, 2).PadLeft(32, '0').ToCharArray();
+            char[] bin2 = Convert.ToString(y, 2).PadLeft(32, '0').ToCharArray();
+
+            int counter = 0;
+
+            for (int i = 0; i < bin2.Length; i++)
+            {
+                if (bin1[i] != bin2[i])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        //Console.WriteLine(HammingDistance(1,4));
+    }
+}
