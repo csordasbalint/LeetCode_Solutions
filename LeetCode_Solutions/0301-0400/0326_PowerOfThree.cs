eb23a0ee4ce9a8c18558d89c3715cbf4da13f1e6
balintcsordaas <balintcsordaas@gmail.com>
2023-02-22 14:36:29 +0100

    326 Solved - Easy

---
+++ LeetCode_Solutions/0301-0400/0326_PowerOfThree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0301_0400
+{
+    internal class _0326_PowerOfThree
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n < 1)
+            {
+                return false;
+            }
+
+            double number = n;
+            while (number >= 1 || number % 3 == 0)
+            {
+                if (number == 1)
+                {
+                    return true;
+                }
+                number /= 3;
+            }
+            return false;
+        }
+
+        //Console.WriteLine(IsPowerOfThree(0));
+        //Console.WriteLine(IsPowerOfThree(1));
+        //Console.WriteLine(IsPowerOfThree(12));
+        //Console.WriteLine(IsPowerOfThree(27));
+    }
+}
