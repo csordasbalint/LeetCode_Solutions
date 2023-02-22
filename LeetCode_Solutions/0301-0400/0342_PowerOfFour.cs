e4a7bf2ff00152d03b06948ad9e14bae2dc85123
balintcsordaas <balintcsordaas@gmail.com>
2023-02-22 14:32:42 +0100

    342 Added previously missed solution

--- LeetCode_Solutions/0301-0400/0342_PowerOfFour.cs
+++ LeetCode_Solutions/0301-0400/0342_PowerOfFour.cs
@@ -8,5 +8,28 @@ namespace LeetCode_Solutions._0301_0400
 {
     internal class _0342_PowerOfFour
     {
+        public bool IsPowerOfFour(int n)
+        {
+            if (n < 1)
+            {
+                return false;
+            }
+
+            double number = n;
+            while (number >= 1 || number % 4 == 0)
+            {
+                if (number == 1)
+                {
+                    return true;
+                }
+                number /= 4;
+            }
+            return false;
+        }
+
+        //Console.WriteLine(IsPowerOfFour(0));
+        //Console.WriteLine(IsPowerOfFour(1));
+        //Console.WriteLine(IsPowerOfFour(9));
+        //Console.WriteLine(IsPowerOfFour(16));
     }
 }
