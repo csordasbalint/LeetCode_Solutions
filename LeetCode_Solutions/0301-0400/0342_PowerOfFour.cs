c31b0451c5fc239d7658b6bf632401f471f757e4
balintcsordaas <balintcsordaas@gmail.com>
2023-02-22 14:30:51 +0100

    342 Solved - Easy

---
+++ LeetCode_Solutions/0301-0400/0342_PowerOfFour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0301_0400
+{
+    internal class _0342_PowerOfFour
+    {
+    }
+}
