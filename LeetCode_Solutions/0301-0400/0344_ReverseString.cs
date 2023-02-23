2f84fa9ced7b66968b1f6217909fbc3cf930400e
balintcsordaas <balintcsordaas@gmail.com>
2023-02-23 15:24:06 +0100

    344 Solved - Easy

---
+++ LeetCode_Solutions/0301-0400/0344_ReverseString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0301_0400
+{
+    internal class _0344_ReverseString
+    {
+        public void ReverseString(char[] s)  //char []
+        {
+            char extraChar;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s.Length - 1 - i < i)
+                {
+                    break;
+                }
+                extraChar = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = s[i];
+                s[i] = extraChar;
+            }
+            //return s
+        }
+
+        //char[] arr = {'A','b','c','d' ,'e', 'a' };
+        //ReverseString(arr);
+        //foreach (var item in arr)
+        //{
+        //	Console.WriteLine(item);
+        //}
+    }
+}
