4f89cc5db5cbaa82737fac27744ce87322dc887a
balintcsordaas <balintcsordaas@gmail.com>
2023-02-25 12:49:25 +0100

    509 Solved - Easy

---
+++ LeetCode_Solutions/0501-0600/0509_FibonacciNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0501_0600
+{
+    internal class _0509_FibonacciNumber
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            int tmp = 0;
+            int current = 1;
+            int previous = 1;
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                tmp = current + previous;
+                previous = current;
+                current = tmp;
+            }
+            return current;
+        }
+
+        //Console.WriteLine(Fib(5));
+
+        //int Fib(int n)
+        //{
+        //	if (n <= 1)
+        //	{
+        //		return n;
+        //	}
+        //	return Fib(n - 2) + Fib(n - 1);
+        //}
+    }
+}
