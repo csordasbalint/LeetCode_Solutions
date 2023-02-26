60182b288ae06b123f01c07789d5a66e8135c814
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 21:26:21 +0100

    905 Solved - Easy

---
+++ LeetCode_Solutions/0901-1000/0905_SortArrayByParity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0901_1000
+{
+    internal class _0905_SortArrayByParity
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    even.Add(nums[i]);
+                }
+                else
+                {
+                    odd.Add(nums[i]);
+                }
+            }
+            even.AddRange(odd);
+            return even.ToArray();
+        }
+
+        //int []numbers = { 3, 1, 2, 4 };
+        //int[] arr = SortArrayByParity(numbers);
+        //foreach (var item in arr)
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
