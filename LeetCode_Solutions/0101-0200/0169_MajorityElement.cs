0cd61336ee2accc7b409b3a44ab1962693dd3b88
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 10:45:15 +0100

    169 Solved - Easy

---
+++ LeetCode_Solutions/0101-0200/0169_MajorityElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0101_0200
+{
+    internal class _0169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var output = from x in nums
+                         group x by x into g
+                         select new
+                         {
+                             tmp = g.Key,
+                             mostFreq = g.Count()
+                         };
+
+            foreach (var item in output)
+            {
+                if (item.mostFreq > nums.Length / 2)
+                {
+                    return item.tmp;
+                }
+            }
+            return 0;
+        }
+
+        //int[] test = { 2, 2, 1, 1, 1, 2, 2 };
+        //Console.WriteLine(MajorityElement(test));
+    }
+}
