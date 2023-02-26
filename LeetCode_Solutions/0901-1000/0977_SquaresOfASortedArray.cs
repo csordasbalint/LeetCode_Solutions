bb0976c190ba5cd5aca2c45d8fd6dc7487657195
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 18:54:18 +0100

    977 Solved - Easy

---
+++ LeetCode_Solutions/0901-1000/0977_SquaresOfASortedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0901_1000
+{
+    internal class _0977_SquaresOfASortedArray
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] * nums[i];
+            }
+            Array.Sort(nums);
+            return nums;
+        }
+
+        //int[] numbers = { -7, -3, 2, 3, 11 };
+        //int[] arr = SortedSquares(numbers);
+        //foreach (var item in arr)
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
