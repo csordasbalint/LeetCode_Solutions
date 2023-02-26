26d3918265582d5866c12fe53eff17d0214da871
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 22:05:48 +0100

    1323 Solved - Easy

---
+++ LeetCode_Solutions/1301-1400/1323_Maximum69Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1301_1400
+{
+    internal class _1323_Maximum69Number
+    {
+        public int Maximum69Number(int num)
+        {
+            char[] arr = num.ToString().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '6')
+                {
+                    arr[i] = '9';
+                    break;
+                }
+            }
+            return Convert.ToInt32(new string(arr));
+        }
+
+        //Console.WriteLine(Maximum69Number(9669));
+    }
+}
