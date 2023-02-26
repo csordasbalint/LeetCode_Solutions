127f529b243237d30ccbc3b5e52dfbd0fe644678
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 19:43:08 +0100

    657 Solved - Easy

---
+++ LeetCode_Solutions/0601-0700/0657_RobotReturnToOrigin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0601_0700
+{
+    internal class _0657_RobotReturnToOrigin
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int numberU = moves.Count(x => x == 'U');
+            int numberD = moves.Count(x => x == 'D');
+            int numberL = moves.Count(x => x == 'L');
+            int numberR = moves.Count(x => x == 'R');
+
+            return (numberU == numberD && numberL == numberR);
+        }
+
+        //Console.WriteLine(JudgeCircle("URRDLDUL"));
+    }
+}
