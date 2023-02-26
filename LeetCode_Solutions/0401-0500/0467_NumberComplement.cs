9686dc5e40718f2ca51b09c2c45e0170af1b9e28
balintcsordaas <balintcsordaas@gmail.com>
2023-02-26 11:22:18 +0100

    467 Solved - Easy

---
+++ LeetCode_Solutions/0401-0500/0467_NumberComplement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0401_0500
+{
+    internal class _0467_NumberComplement
+    {
+        public int FindComplement(int num)
+        {
+            char[] arr = Convert.ToString(num, 2).ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '0')
+                {
+                    arr[i] = '1';
+                }
+                else
+                {
+                    arr[i] = '0';
+                }
+            }
+            return Convert.ToInt32(new string(arr), 2);
+        }
+
+        //Console.WriteLine(FindComplement(5));
+    }
+}
