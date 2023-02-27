31f8393046bb93acc2012e5d332a9fb9e560c520
balintcsordaas <balintcsordaas@gmail.com>
2023-02-27 23:47:42 +0100

    1281 Solved - Easy

---
+++ LeetCode_Solutions/1201-1300/1281_SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1201_1300
+{
+    internal class _1281_SubtractTheProductAndSumOfDigitsOfAnInteger
+    {
+        int SubtractProductAndSum(int n)
+        {
+            char[] arr = n.ToString().ToCharArray();
+            int product = 1;
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                product *= arr[i] - 48;
+                sum += arr[i] - 48;
+            }
+            return product - sum;
+        }
+
+
+        //Console.WriteLine(SubtractProductAndSum(234));
+    }
+}
