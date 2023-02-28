b342193bda68be42b57994cff9c5886fb1d5654f
balintcsordaas <balintcsordaas@gmail.com>
2023-02-28 14:47:12 +0100

    217 Solved - Easy

---
+++ LeetCode_Solutions/0201-0300/0217_ContainsDuplicates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0201_0300
+{
+    internal class _0217_ContainsDuplicates
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (nums.Distinct().Count() == nums.Count())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        //int[] arr = { 1, 2, 3, 1 };
+        //Console.WriteLine(ContainsDuplicate(arr));
+    }
+}
