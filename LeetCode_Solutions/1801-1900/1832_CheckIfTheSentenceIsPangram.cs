9d5f257045419bc4d76e5432c5951c59a77923dd
balintcsordaas <balintcsordaas@gmail.com>
2023-02-28 12:57:21 +0100

    1832 Solved - Easy

---
+++ LeetCode_Solutions/1801-1900/1832_CheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1801_1900
+{
+    internal class _1832_CheckIfTheSentenceIsPangram
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                if (!sentence.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //Console.WriteLine(CheckIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+    }
+}
