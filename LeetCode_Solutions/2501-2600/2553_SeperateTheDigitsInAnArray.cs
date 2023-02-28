e16e0225a11fd2e509f1a76ce04b043f663a8332
balintcsordaas <balintcsordaas@gmail.com>
2023-02-28 22:07:55 +0100

    2553 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2553_SeperateTheDigitsInAnArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2553_SeperateTheDigitsInAnArray
+    {
+        public int[] SeparateDigits(int[] nums)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sb.Append(nums[i]);
+            }
+            int[] output = sb.ToString().Select(x => x - 48).ToArray();
+
+            return output;
+        }
+
+
+        //int[] numbers = { 13, 25, 83, 77 };
+        //int[] tmp = SeparateDigits(numbers);
+        //foreach (var item in tmp)
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
