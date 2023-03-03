6db1fa8212ff6c0f5fcac38e9c45aecd0502e963
balintcsordaas <balintcsordaas@gmail.com>
2023-03-03 12:47:24 +0100

    2544 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2544_AlternatingDigitSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2544_AlternatingDigitSum
+    {
+        public int AlternateDigitSum(int n)
+        {
+            int output = 0;
+            char[] arr = n.ToString().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    output += (arr[i] - 48);
+                }
+                else
+                {
+                    output -= (arr[i] - 48);
+                }
+            }
+            return output;
+        }
+
+
+        //Console.WriteLine(AlternateDigitSum(521));
+        //Console.WriteLine(AlternateDigitSum(111));
+        //Console.WriteLine(AlternateDigitSum(886996));
+    }
+}
