27fa168dc96ff0c2b499cea83bde44f67e5d4430
balintcsordaas <balintcsordaas@gmail.com>
2023-03-04 23:10:37 +0100

    2124 Solved - Easy

---
+++ LeetCode_Solutions/2101-2200/2124_CheckIfAllA'sAppearsBeforeAllB's.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2101_2200
+{
+    internal class _2124_CheckIfAllA_sAppearsBeforeAllB_s
+    {
+        public bool CheckString(string s)
+        {
+            int idx = s.IndexOf("b");
+            if (idx == -1)
+            {
+                return true;
+            }
+            string subString = s.Substring(idx, s.Length - idx);
+            if (subString.Contains("a"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Console.WriteLine(CheckString("a"));
+        //Console.WriteLine(CheckString("aaabbb"));
+        //Console.WriteLine(CheckString("abbbba"));
+    }
+}
