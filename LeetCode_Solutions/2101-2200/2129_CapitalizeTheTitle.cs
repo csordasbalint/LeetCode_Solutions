47fb482cc78f843d1a639abf536645605f143021
balintcsordaas <balintcsordaas@gmail.com>
2023-03-04 22:57:50 +0100

    2129 Solved - Easy

---
+++ LeetCode_Solutions/2101-2200/2129_CapitalizeTheTitle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2101_2200
+{
+    internal class _2129_CapitalizeTheTitle
+    {
+        public string CapitalizeTitle(string title)
+        {
+            char[] tmp;
+            string text = title.ToLower();
+            string[] shards = text.Split(" ");
+
+            for (int i = 0; i < shards.Length; i++)
+            {
+                if (shards[i].Length > 2)
+                {
+                    tmp = shards[i].ToCharArray();
+                    tmp[0] = char.ToUpper(tmp[0]);
+                    shards[i] = new string(tmp);
+                }
+            }
+            string output = string.Join(" ", shards);
+            return output;
+        }
+
+        //Console.WriteLine(CapitalizeTitle("capiTalIze tHe titLe"));
+    }
+}
