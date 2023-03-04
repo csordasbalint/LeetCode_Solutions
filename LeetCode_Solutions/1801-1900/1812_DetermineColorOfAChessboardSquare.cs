6348d4c94f0036d8db7762114d4d1b3e8a3015fc
balintcsordaas <balintcsordaas@gmail.com>
2023-03-04 13:21:38 +0100

    1812 Solved - Easy

---
+++ LeetCode_Solutions/1801-1900/1812_DetermineColorOfAChessboardSquare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1801_1900
+{
+    internal class _1812_DetermineColorOfAChessboardSquare
+    {
+        public bool SquareIsWhite(string coordinates)
+        {
+            if ((coordinates[0] + coordinates.Last()) % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Console.WriteLine(SquareIsWhite("h3"));
+    }
+}
