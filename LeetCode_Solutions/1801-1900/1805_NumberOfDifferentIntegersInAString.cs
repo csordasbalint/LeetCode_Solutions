9f40407fa2f482f285a1c5605acce58ac158b35f
balintcsordaas <balintcsordaas@gmail.com>
2023-03-04 14:52:20 +0100

    1805 Solved - Easy

---
+++ LeetCode_Solutions/1801-1900/1805_NumberOfDifferentIntegersInAString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1801_1900
+{
+    internal class _1805_NumberOfDifferentIntegersInAString
+    {
+        public int NumDifferentIntegers(string word)
+        {
+            string tmp = "";
+            char[] arr = word.ToCharArray();
+            List<BigInteger> list = new List<BigInteger>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!char.IsDigit(arr[i]))
+                {
+                    arr[i] = ' ';
+                }
+                else
+                {
+                    while (i < arr.Length && char.IsDigit(arr[i]))
+                    {
+                        tmp += arr[i];
+                        i++;
+                    }
+                    list.Add(BigInteger.Parse(tmp));
+                    tmp = "";
+                    i--;
+                }
+            }
+            return list.Distinct().Count();
+        }
+
+        //Console.WriteLine(NumDifferentIntegers("a123bc34d8ef34"));
+    }
+}
