36ed4e961b72401fe19bf93db6f66fc4bb21b786
balintcsordaas <balintcsordaas@gmail.com>
2023-03-05 21:13:48 +0100

    2119 Solved - Easy

---
+++ LeetCode_Solutions/2101-2200/2119_ANumberAfterADoubleReversal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2101_2200
+{
+    internal class _2119_ANumberAfterADoubleReversal
+    {
+        public bool IsSameAfterReversals(int num)
+        {
+            char[] arr = num.ToString().ToArray();
+            Array.Reverse(arr);
+            int tmp = int.Parse(arr);
+
+            char[] arr2 = tmp.ToString().ToCharArray();
+            Array.Reverse(arr2);
+            int tmp2 = int.Parse(arr2);
+            if (tmp2 == num)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        //Console.WriteLine(IsSameAfterReversals(0));
+        //Console.WriteLine(IsSameAfterReversals(526));
+        //Console.WriteLine(IsSameAfterReversals(1800));
+    }
+}
