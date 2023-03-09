5e3d2a938782e81e6f75bfa8f34c6c4219b4f0b7
balintcsordaas <balintcsordaas@gmail.com>
2023-03-09 11:56:08 +0100

    2582 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2582_PassThePillow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2582_PassThePillow
+    {
+        public int PassThePillow(int n, int time)
+        {
+            int current = 1;
+            bool changeDir = true;
+            while (time != 0)
+            {
+                if (changeDir)
+                {
+                    current++;
+                    time--;
+                    if (current == n)
+                    {
+                        changeDir = false;
+                    }
+                }
+                else
+                {
+                    current--;
+                    if (current == 1)
+                    {
+                        changeDir = true;
+                    }
+                    time--;
+                }
+            }
+            return current;
+        }
+
+        //Console.WriteLine(PassThePillow(4,5));
+        //Console.WriteLine(PassThePillow(3,2));
+    }
+}
