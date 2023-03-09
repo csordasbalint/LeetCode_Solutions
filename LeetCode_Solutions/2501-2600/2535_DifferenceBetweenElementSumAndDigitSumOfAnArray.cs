5fb9feeccf42d7c4921a24a80f101d37f32bc12e
balintcsordaas <balintcsordaas@gmail.com>
2023-03-09 11:34:29 +0100

    2535 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2535_DifferenceBetweenElementSumAndDigitSumOfAnArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2535_DifferenceBetweenElementSumAndDigitSumOfAnArray
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            string tmp;
+            int digitSum = 0;
+            int elementSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                elementSum += nums[i];
+                if (nums[i] > 9)
+                {
+                    tmp = nums[i].ToString();
+                    for (int j = 0; j < tmp.Length; j++)
+                    {
+                        digitSum += Convert.ToInt32(tmp[j]) - 48;
+                    }
+                }
+                else
+                {
+                    digitSum += nums[i];
+                }
+            }
+            return Math.Abs(elementSum - digitSum);
+        }
+
+
+        //int[] tmp = { 1, 15, 6, 3 };
+        //Console.WriteLine(DifferenceOfSum(tmp));
+    }
+}
