a52129373c1b5cefacaafc471cfc4f86e94407cc
balintcsordaas <balintcsordaas@gmail.com>
2023-03-09 10:51:51 +0100

    1009 Solved - Easy

---
+++ LeetCode_Solutions/1001-1100/ComplementOfBase10Integer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1001_1100
+{
+    internal class ComplementOfBase10Integer
+    {
+        public int BitwiseComplement(int n)
+        {
+            string toBinary = Convert.ToString(n, 2);
+            char[] arr = toBinary.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '0')
+                {
+                    arr[i] = '1';
+                }
+                else
+                {
+                    arr[i] = '0';
+                }
+            }
+
+            string toConvert = new string(arr);
+            return Convert.ToInt32(toConvert, 2);
+        }
+
+
+        //Console.WriteLine(BitwiseComplement(5));
+    }
+}
