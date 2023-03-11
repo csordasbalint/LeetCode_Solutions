af99ec084ac405fda51405c4f2060f45abf15284
balintcsordaas <balintcsordaas@gmail.com>
2023-03-11 21:56:15 +0100

    1360 Solved - Easy

---
+++ LeetCode_Solutions/1301-1400/1360_NumberOfDaysBetweenTwoDates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1301_1400
+{
+    internal class _1360_NumberOfDaysBetweenTwoDates
+    {
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            TimeSpan diff = DateTime.Parse(date2) - DateTime.Parse(date1);
+            return Math.Abs((int)diff.TotalDays);
+        }
+
+
+        //Console.WriteLine(DaysBetweenDates("2019-06-29", "2019-06-30"));
+        //Console.WriteLine(DaysBetweenDates("2020-01-15", "2019-12-31"));
+        //Console.WriteLine(DaysBetweenDates("2020-02-29", "2021-05-15"));
+        //Console.WriteLine(DaysBetweenDates("1999-12-31", "2000-01-01"));
+    }
+}
