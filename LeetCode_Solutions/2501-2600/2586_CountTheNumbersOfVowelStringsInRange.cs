611733a09f9b439123830a6d4f7c32fd4cc73299
balintcsordaas <balintcsordaas@gmail.com>
2023-03-12 10:54:35 +0100

    2586 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2586_CountTheNumbersOfVowelStringsInRange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2586_CountTheNumbersOfVowelStringsInRange
+    {
+        public int VowelStrings(string[] words, int left, int right)
+        {
+            string vowels = "aeiou";
+            int counter = 0;
+            for (int i = left; i < right + 1; i++)
+            {
+                if (vowels.Contains(words[i][0]) && vowels.Contains(words[i][words[i].Length - 1]))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+
+        //string [] arr = { "n", "a", "o" };
+        //string[] arr2 = { "are", "amy", "u" };
+        //Console.WriteLine(VowelStrings(arr,0,2));
+    }
+}
