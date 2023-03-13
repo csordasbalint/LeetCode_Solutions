a7690c8a1c9b98a647d14f86cc32a740babef905
balintcsordaas <balintcsordaas@gmail.com>
2023-03-13 01:01:04 +0100

    1689 Solved - Medium

---
+++ LeetCode_Solutions/1601-1700/1689_PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1601_1700
+{
+    internal class _1689_PartitioningIntoMinimumNumberOfDeci_BinaryNumbers
+    {
+        public int MinPartitions(string n)
+        {
+            char[] arr = n.ToCharArray();
+            return arr.Max() - 48;
+        }
+    }
+}
