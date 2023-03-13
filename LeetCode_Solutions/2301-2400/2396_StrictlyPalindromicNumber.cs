afd4163ed6f5f3e705461c8c52c9928df395f751
balintcsordaas <balintcsordaas@gmail.com>
2023-03-13 11:30:08 +0100

    2396 Solved - Medium

---
+++ LeetCode_Solutions/2301-2400/2396_StrictlyPalindromicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2301_2400
+{
+    internal class _2396_StrictlyPalindromicNumber
+    {
+        public bool IsStrictlyPalindromic(int n)
+        {
+            string binary;
+            for (int i = 2; i <= n - 2; i++)
+            {
+                binary = Convert.ToString(n, i);
+                if (binary != binary.Reverse())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //Console.WriteLine(IsStrictlyPalindromic(9));
+        //Console.WriteLine(IsStrictlyPalindromic(4));
+    }
+}
