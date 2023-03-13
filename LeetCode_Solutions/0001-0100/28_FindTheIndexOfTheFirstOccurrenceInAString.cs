9307e3feded28e25677d3f424129b805fe38085e
balintcsordaas <balintcsordaas@gmail.com>
2023-03-13 21:47:25 +0100

    28 Solved - Easy

---
+++ LeetCode_Solutions/0001-0100/28_FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0001_0100
+{
+    internal class _28_FindTheIndexOfTheFirstOccurrenceInAString
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack.Contains(needle))
+            {
+                return haystack.IndexOf(needle);
+            }
+            return -1;
+        }
+
+
+        //Console.WriteLine(StrStr("sadbutsad", "sad"));
+        //Console.WriteLine(StrStr("leetcode", "leeto"));
+    }
+}
