c91a15269405a8d1e06d9685e62dfdc078102e1f
balintcsordaas <balintcsordaas@gmail.com>
2023-03-15 15:38:04 +0100

    58 Solved - Easy

---
+++ LeetCode_Solutions/0001-0100/0058_LengthOfLastWord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._0001_0100
+{
+    internal class _0058_LengthOfLastWord
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            List<string> list = s.Split(" ").ToList();
+            return list.Last().Length;
+        }
+
+
+        //Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+        //Console.WriteLine(LengthOfLastWord("luffy is still joyboy"));
+    }
+}
