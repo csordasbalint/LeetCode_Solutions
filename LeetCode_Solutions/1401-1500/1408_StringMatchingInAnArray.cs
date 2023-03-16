dd54af12eb87413fdecdfddb538a2dcb2df2a55a
balintcsordaas <balintcsordaas@gmail.com>
2023-03-16 19:49:15 +0100

    1408 Solved - Easy

---
+++ LeetCode_Solutions/1401-1500/1408_StringMatchingInAnArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._1401_1500
+{
+    internal class _1408_StringMatchingInAnArray
+    {
+        public IList<string> StringMatching(string[] words)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (i != j && words[j].Contains(words[i]))
+                    {
+                        if (!list.Contains(words[i]))
+                        {
+                            list.Add(words[i]);
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        //string[] arr = { "mass", "as", "hero", "superhero" };
+        //IList<string> outputStrings = StringMatching(arr);
+        //foreach (var item in outputStrings)
+        //{
+        //    Console.WriteLine(item);
+        //}
+    }
+}
