a7903936fcb4c39f4d4a970dd59915f23faf82c9
balintcsordaas <balintcsordaas@gmail.com>
2023-03-21 23:33:44 +0100

    2278 Solved - Easy

---
+++ LeetCode_Solutions/2201-2300/2278_PercentageOfLetterInString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2201_2300
+{
+    internal class _2278_PercentageOfLetterInString
+    {
+        public int PercentageLetter(string s, char letter)
+        {
+            return s.Count(x => x == letter) * 100 / (s.Length);
+        }
+
+
+        //Console.WriteLine(PercentageLetter("foobar",'o'));
+    }
+}
