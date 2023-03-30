9be7691f0d8dceca73534ed92a4103adf074ca41
balintcsordaas <balintcsordaas@gmail.com>
2023-03-30 23:48:45 +0200

    2600 Solved - Easy

---
+++ LeetCode_Solutions/2501-2600/2600_KItemsWithTheMaximumSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions._2501_2600
+{
+    internal class _2600_KItemsWithTheMaximumSum
+    {
+        public int KItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k)
+        {
+            int output = 0;
+            int[] arr = new int[numOnes + numZeros + numNegOnes];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < numOnes && i < k)
+                {
+                    arr[i] = 1;
+                    output += arr[i];
+                }
+                else if (i < numOnes + numZeros && i < k)
+                {
+                }
+                else if (i < k)
+                {
+                    arr[i] = -1;
+                    output += arr[i];
+                }
+            }
+            return output;
+        }
+
+        //Console.WriteLine(KItemsWithMaximumSum(3, 2, 0, 2));
+        //Console.WriteLine(KItemsWithMaximumSum(2, 5, 0, 3));
+        //Console.WriteLine(KItemsWithMaximumSum(1, 1, 3, 5));
+        //Console.WriteLine(KItemsWithMaximumSum(6, 6, 6, 13));
+        //Console.WriteLine(KItemsWithMaximumSum(1, 3, 3, 5));
+        //Console.WriteLine(KItemsWithMaximumSum(1, 1, 1, 3));
+    }
+}
